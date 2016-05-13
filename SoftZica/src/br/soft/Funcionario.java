679816408c7ae0c8106de435cfb9cd6cae1834e4
Vanessa Panosso <panossovanessa@gmail.com>
2016-05-13 18:35:42 -0300

    Getter e Setter classe Funcionario

--- SoftZica/src/br/soft/Funcionario.java
+++ SoftZica/src/br/soft/Funcionario.java
@@ -6,4 +6,24 @@ public class Funcionario {
 	private String nome;
 	private String cpf;
 	private Date nascimento;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public Date getNascimento() {
+		return nascimento;
+	}
+	public void setNascimento(Date nascimento) {
+		this.nascimento = nascimento;
+	}
+	
+	
 }
